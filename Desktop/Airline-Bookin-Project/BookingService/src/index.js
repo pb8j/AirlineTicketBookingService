8b71b5d484172a49693156c3e815c779ad23c566
Puru Brahmankar <purubrahmankar8@gmail.com>
2025-02-16 22:24:45 +0530

    Completed create Booking

--- Desktop/Airline-Bookin-Project/BookingService/src/index.js
+++ Desktop/Airline-Bookin-Project/BookingService/src/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-const {DB_SYNC} = require('./config/serverConfig')
-const {PORT} = require('./config/serverConfig');
+const {DB_SYNC } = require('./config/serverConfig');
+const {PORT,FLIGHT_SERVICE_PATH} = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
 const db = require('./models/index');
 
@@ -19,6 +19,7 @@ const setupAndStartServer = () => {
         if(process.env.DB_SYNC) {
             db.sequelize.sync({alter: true});
         }
+        console.log(FLIGHT_SERVICE_PATH);
     });
 }
 
